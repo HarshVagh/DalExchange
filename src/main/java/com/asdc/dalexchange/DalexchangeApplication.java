85ac5f05fd280089f1914d564585f40bf967c7ed
hvaghasiya <harsh.vaghasiya@dal.ca>
2024-05-27 21:59:33 -0300

    intial spring-boot project setup

---
+++ src/main/java/com/asdc/dalexchange/DalexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.asdc.dalexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalexchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalexchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asdc/dalexchange/DalexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asdc.dalexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalexchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
