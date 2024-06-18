aa560e45605ea73e76a47f98258597d3e909f278
Shivani Uppe <sh577665@dal.ca>
2024-06-18 00:15:10 -0300

    Created UI for register page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import ProductList from './features/priduct_list/ProductList';
 import Login from './features/authentication/Login'
+import Signup from './features/authentication/Signup';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         {/* <Route path="/" element={ <Home/> } /> */}
         <Route path="login" element={ <Login/> } />
+        <Route path="signup" element={ <Signup/> } />
         <Route path="products" element={ <ProductList/> } />
       </Routes>
     </div>
