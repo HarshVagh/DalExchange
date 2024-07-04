0cb086de8fbe3c0e7d3e747a308d223b4bb06b70
hvaghasiya <harsh.vaghasiya@dal.ca>
2024-07-04 01:54:20 -0300

    implemented pagination, searching and filtering feature in react

--- frontend/src/features/product_list/ProductList.jsx
+++ frontend/src/features/product_list/ProductList.jsx
@@ -1,13 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { ProductListingAPI } from "../../services/productListingApi";
-import Header from "../../components/header";
+import Header from "../../components/Header";
 import ProductCard from "./components/ProductCard";
 import Sidebar from "./components/Sidebar";
+import Pagination from "./components/Pagination";
 
 const ProductList = () => {
   const [products, setProducts] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [search, setSearch] = useState("");
+  const [pageData, setPageData] = useState({
+    page: 0,
+    size: 12,
+    totalElements: 12,
+    totalPages: 1
+  });
+
+  const [filters, setFilters] = useState({
+    minPrice: -1,
+    maxPrice: -1,
+    categories: [],
+    conditions: []
+  });
+  
   const headerConfig = {
     search: true,
     requests: true,
@@ -20,22 +36,69 @@ const ProductList = () => {
     fetchProducts();
   }, []);
 
-  const fetchProducts = async () => {
-    await ProductListingAPI.get(setProducts, setIsLoading, setError);
+  const fetchProducts = async (page = pageData.page) => {
+    const params = {
+      page: page,
+      size: pageData.size
+    }
+    if(search && search.length > 0) {
+      params["search"] = search;
+    }
+    if(filters.minPrice !== -1) {
+      params["minPrice"] = filters.minPrice;
+    }
+    if(filters.maxPrice !== -1) {
+      params["maxPrice"] = filters.maxPrice;
+    } 
+    if(filters.categories.length > 0) {
+      params["categories"] = filters.categories;
+    } 
+    if(filters.conditions.length > 0) {
+      params["conditions"] = filters.conditions;
+    }
+    const setters = {
+      products: setProducts,
+      isLoading: setIsLoading,
+      error: setError,
+      pageData: setPageData
+    };
+    console.log("request params: ", params)
+    await ProductListingAPI.get(setters, params);
+  };
+
+  const handlePageChange = (newPage) => {
+    fetchProducts(newPage);
+  };
+
+  const handleSearchSubmit = () => {
+    fetchProducts();
+  };
+
+  const handleFilterSubmit = () => {
+    fetchProducts();
+  };
+
+  const handleFilterUpdate = (updatedfilters) => {
+    setFilters(updatedfilters);
   };
 
   return (
     !isLoading && <div className="flex flex-col h-screen">
-      <Header config={headerConfig} />
+      <Header 
+        config={headerConfig} 
+        search={search}
+        setSearch={setSearch}
+        onSearchSubmit={handleSearchSubmit} />
       <div className="lg:flex lg:flex-1 md:flex md:flex-1">
         
-        <Sidebar></Sidebar>
+        <Sidebar 
+          filters={filters}
+          onFilterUpdate={handleFilterUpdate} 
+          onFilterSubmit={handleFilterSubmit}>
+        </Sidebar>
         
         {error && <div className="flex justify-center h-16 w-full" >
           <div className="flex items-center py-4 px-12 mt-4 text-sm text-red-600 rounded-lg bg-red-50 border-2 border-red-600" role="alert">
-            <svg className="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-            </svg>
             <span className="sr-only">Error</span>
             <div>
               <span className="font-medium">Error!</span> {error.message}
@@ -58,7 +121,11 @@ const ProductList = () => {
                 ></ProductCard>
               ))}
           </div>
-        </div>}
+
+          {pageData.totalPages > 1 && <div className="flex justify-center mt-4 mb-4">
+              <Pagination pageData={pageData} onPageChange={handlePageChange}></Pagination>
+          </div>}
+        </div>}        
       </div>
     </div>
   );
