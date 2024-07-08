dacfb3cd9fbb23ba59091b4be1d980430fd554cc
rvaghani <rt691750@dal.ca>
2024-07-07 21:37:51 -0300

    saved product API is created

--- backend/src/main/java/com/asdc/dalexchange/controller/ProfilePageController.java
+++ backend/src/main/java/com/asdc/dalexchange/controller/ProfilePageController.java
@@ -2,10 +2,13 @@ package com.asdc.dalexchange.controller;
 
 
 import com.asdc.dalexchange.dto.ProfilePageDTO;
+import com.asdc.dalexchange.dto.SavedProductDTO;
 import com.asdc.dalexchange.service.ProfilePageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -18,5 +21,11 @@ public class ProfilePageController {
     public ResponseEntity<ProfilePageDTO> profilledetails(@PathVariable Long userId) {
         ProfilePageDTO ProfilePageDTOs = profilePageService.ProfileDetails(userId);
         return ResponseEntity.ok(ProfilePageDTOs);
+    }
+
+    @GetMapping("/saved_products/{userId}")
+    public ResponseEntity<List<SavedProductDTO>> getAllSavedProducts(@PathVariable Long userId) {
+        List<SavedProductDTO> savedProductDTOs = profilePageService.GetAllsavedProduct(userId);
+        return ResponseEntity.ok(savedProductDTOs);
     }
 }
--- backend/src/main/java/com/asdc/dalexchange/service/ProfilePageService.java
+++ backend/src/main/java/com/asdc/dalexchange/service/ProfilePageService.java
@@ -1,6 +1,7 @@
 package com.asdc.dalexchange.service;
 
 import com.asdc.dalexchange.dto.ProfilePageDTO;
+import com.asdc.dalexchange.dto.SavedProductDTO;
 import com.asdc.dalexchange.model.User;
 
 import java.util.List;
@@ -9,5 +10,8 @@ public interface ProfilePageService {
 
     //home page ddetails of the user profilepage
     ProfilePageDTO ProfileDetails(Long userid);
+
+    // get the all saved items
+    List<SavedProductDTO> GetAllsavedProduct(Long userid);
 
 }
