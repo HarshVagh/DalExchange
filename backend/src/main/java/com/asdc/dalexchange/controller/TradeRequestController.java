b6a9158154c7b604b75410be7a024c65262b0e99
hvaghasiya <harsh.vaghasiya@dal.ca>
2024-07-09 21:25:11 -0300

    removed unused import

--- backend/src/main/java/com/asdc/dalexchange/controller/TradeRequestController.java
+++ backend/src/main/java/com/asdc/dalexchange/controller/TradeRequestController.java
@@ -7,7 +7,6 @@ import com.asdc.dalexchange.service.TradeRequestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
