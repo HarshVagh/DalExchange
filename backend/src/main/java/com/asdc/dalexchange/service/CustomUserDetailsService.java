754b6b64cee280f217d5a20449720ee00102f46b
Shivani Uppe <sh577665@dal.ca>
2024-07-14 18:30:01 -0300

    interfaces implemented for CustomUserDetailsService and EmailService and changed test cases

--- backend/src/main/java/com/asdc/dalexchange/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/asdc/dalexchange/service/CustomUserDetailsService.java
@@ -1,16 +1,9 @@
 package com.asdc.dalexchange.service;
 
-
-import com.asdc.dalexchange.model.User;
-import com.asdc.dalexchange.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
 
-@Service
-public interface CustomUserDetailsService {
-
-     UserDetails loadUserByUsername(String email);
+public interface CustomUserDetailsService extends UserDetailsService {
+     UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
 }
