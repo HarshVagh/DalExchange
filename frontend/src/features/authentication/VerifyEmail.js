66bdda4e3959b10e3cf97e5c4fe265ad1abc4824
Shivani Uppe <sh577665@dal.ca>
2024-07-16 01:51:55 -0300

    SignUp UI new fields added and modified the API accordingly

--- frontend/src/features/authentication/VerifyEmail.js
+++ frontend/src/features/authentication/VerifyEmail.js
@@ -54,9 +54,9 @@ const VerifyEmail = () => {
                         <div className="mb-6">
                             <label htmlFor="code" className="block mb-2 text-sm font-medium text-gray-900">Verification Code</label>
                             <input
-                                type="code"
+                                type="text"
                                 id="code"
-                                value={String}
+                                value={code}
                                 onChange={(e) => setCode(e.target.value)}
                                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                 required
