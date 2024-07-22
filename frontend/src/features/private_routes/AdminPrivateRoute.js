28e0a00aadcf6763e4c90121555370e68eaecd25
Shivani Uppe <sh577665@dal.ca>
2024-07-21 22:25:37 -0300

    Created react API for StudentPrivateRoute and AdminPrivateRoute components

--- frontend/src/features/private_routes/AdminPrivateRoute.js
+++ frontend/src/features/private_routes/AdminPrivateRoute.js
@@ -1,33 +1,42 @@
-import React, { useEffect } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
-import AuthenticationApi from '../../services/AuthenticationApi';
+import PrivateRoutesApi from '../../services/PrivateRoutesApi';
 
 const AdminPrivateRoute = ({ children }) => {
     const isAuthenticated = !!localStorage.getItem('jwtToken');
     const { user, setUser } = useUser();
-    const navigate = useNavigate();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const setupUser = async () => {
             const token = localStorage.getItem('jwtToken');
             if (token && !user) {
                 try {
-                    const userResponse = await AuthenticationApi.currentUser(token);
+                    const userResponse = await PrivateRoutesApi.currentUser(token);
                     setUser(userResponse.data);
                 } catch (error) {
                     console.error('Failed to fetch current user', error);
+                } finally {
+                    setLoading(false);
                 }
+            } else {
+                setLoading(false);
             }
         };
         setupUser();
-    }, [navigate, user, setUser]);
+    }, [user, setUser]);
+
+    if (loading) {
+        return <div>Loading...</div>; 
+    }
 
     if (!isAuthenticated) return <Navigate to="/login" />;
-
+    
     if (user && user.role !== 'admin') return <Navigate to="/not-authorized" />;
 
     return children;
 };
 
 export default AdminPrivateRoute;
+
