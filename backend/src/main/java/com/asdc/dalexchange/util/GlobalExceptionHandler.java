79d9a820298b0c884973eb1ae02175808beadec6
Shivani Uppe <sh577665@dal.ca>
2024-07-28 00:19:21 -0300

    Added comments

--- backend/src/main/java/com/asdc/dalexchange/util/GlobalExceptionHandler.java
+++ backend/src/main/java/com/asdc/dalexchange/util/GlobalExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GolableExceptionHandler {
+public class GlobalExceptionHandler {
 
 
     @ExceptionHandler(ResourceNotFoundException.class)
